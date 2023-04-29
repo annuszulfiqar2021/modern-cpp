1cbedd7846d1b2d3a0276c398f9f6b9efde37a14
Annus Zulfiqar <annusmanarchitect@gmail.com>
2023-04-28 23:41:01 -0400

    .

--- main.cpp
+++ main.cpp
@@ -4,5 +4,8 @@
 int main() {
     auto result = (10 <=> 20) > 0;
     std::cout << "Result of (10 <=> 20) > 0 = " << result << std::endl;
+    int x = 0;
+    for (int i=0; i<1000000000; i++) x++;
+    std::cout << "Iterated a billion times; X = " << x << std::endl;
     return 0;
 }
