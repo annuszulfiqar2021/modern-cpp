f52d84d313eed86c5899aa0981926c04f64fc2bd
Annus Zulfiqar <annusmanarchitect@gmail.com>
2023-04-29 01:11:05 -0400

    .

--- main.cpp
+++ main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <string>
+#include <memory>
 
 
-int main() {
-    auto result = (10 <=> 20) > 0;
-    std::cout << "Result of (10 <=> 20) > 0 = " << result << std::endl;
-    int x = 0;
-    for (int i=0; i<1000000000; i++) x++;
-    std::cout << "Iterated a billion times; X = " << x << std::endl;
+class Person {
+private:
+    std::string _name;
+public:
+    Person(std::string name) {
+        _name = name;
+        std::cout << _name << " created" << std::endl;
+    }
+    ~Person() {
+        std::cout << _name << " destroyed" << std::endl;
+    }
+    std::string get_name() {
+        return _name;
+    }
+};
+
+
+int main(int argc, char** argv) {
+    // std::unique_ptr<Person> pointer_to_annus = std::make_unique<Person>("Annus Zulfiqar");
+    // std::cout << "My (indirect) name is " << pointer_to_annus->get_name() << std::endl;
+
+    std::shared_ptr<Person> shared_pointer_to_annus = std::make_shared<Person>("Annus Zulfiqar");
+    std::cout << "My (indirect) name is " << shared_pointer_to_annus->get_name() << std::endl;
+
+    std::shared_ptr<Person> shared_pointer_to_shared_pointer_to_annus = shared_pointer_to_annus;
+    std::cout << "My (indirect-indirect) name is " << shared_pointer_to_shared_pointer_to_annus->get_name() << std::endl;
+
     return 0;
 }
