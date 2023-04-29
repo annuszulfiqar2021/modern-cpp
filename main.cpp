2067d79dda28e136ed820bec3dc15be18c92a611
Annus Zulfiqar <annusmanarchitect@gmail.com>
2023-04-29 01:26:33 -0400

    .

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ public:
 };
 
 
-int main(int argc, char** argv) {
+int test(int argc, char** argv) {
     // std::unique_ptr<Person> pointer_to_annus = std::make_unique<Person>("Annus Zulfiqar");
     // std::cout << "My (indirect) name is " << pointer_to_annus->get_name() << std::endl;
 
@@ -30,5 +30,28 @@ int main(int argc, char** argv) {
     std::shared_ptr<Person> shared_pointer_to_shared_pointer_to_annus = shared_pointer_to_annus;
     std::cout << "My (indirect-indirect) name is " << shared_pointer_to_shared_pointer_to_annus->get_name() << std::endl;
 
+    return 0;
+}
+
+
+int vector_sum(const std::vector<int> &vec) {
+    int result = 0;
+    for (int val: vec) {
+        result += val;
+    }
+    return result;
+}
+
+ 
+int main(int argc, char** argv) {
+    int LIMIT = 100000000;
+    std::vector<int> test_vec(LIMIT);
+    std::cout << "Beginning to populate the vector.." << std::endl;
+    for (int i=1; i<=LIMIT; i++) {
+        test_vec.push_back(i);
+    }
+    std::cout << "Vector populated.." << std::endl;
+    std::cout << "Sum of the vector = " << vector_sum(test_vec) << std::endl;
+    std::cout << "Actual sum of the vector = " << int(LIMIT*(LIMIT+1)/2) << std::endl;
     return 0;
 }
